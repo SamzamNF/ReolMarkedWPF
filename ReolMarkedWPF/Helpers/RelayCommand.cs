9636a2f3dedc1f4516f9bea61a71ca3da5a39168
Nicolai Friis <nmfr71398@edu.ucl.dk>
2025-09-17 14:24:34 +0200

    Oprettelse af mapper for en god struktur Oprettelse af Helper klasser til brug af knapper/INotifyPropertyChanged

---
+++ ReolMarkedWPF/Helpers/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ReolMarkedWPF.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        // Felter
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        // Konstruktør
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        // Sørger for, at UI-elementer som knapper automatisk bliver enabled/disabled baseret på CanExecute-logikken.
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value;}
+        }
+
+        // CanExecute-metoden, der tjekker om kommandoen kan udføres.
+        // Hvis _canExecute er null, returneres der true, hvilket betyder, at kommandoen altid kan udføres.
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null | _canExecute(parameter);
+        }
+
+        // Execute-metoden som der kalder den faktiske handling(AddCustomer fx), når kommandoen bliver udført.
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ ReolMarkedWPF/Helpers/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarkedWPF.Helpers
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        // Interface fra PropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //Metode til at aktivere det event fra interfaced
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
