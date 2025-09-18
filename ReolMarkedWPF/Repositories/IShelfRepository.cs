370c892d5bef965e1c86477f21e87d2d2aa541b5
Freja <fbfl71372@edu.ucl.dk>
2025-09-18 15:48:25 +0200

    Interface oprettet.

--- ReolMarkedWPF/Repositories/IShelfRepository.cs
+++ ReolMarkedWPF/Repositories/IShelfRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReolMarkedWPF.Model;
 
 namespace ReolMarkedWPF.Repositories
 {
     internal interface IShelfRepository
     {
+        List<Shelf> GetAllShelves();
+        void AddShelf(Shelf shelf);
+        void DeleteShelf(Shelf shelf);
+        void UpdateShelf(Shelf shelf);
     }
 }
