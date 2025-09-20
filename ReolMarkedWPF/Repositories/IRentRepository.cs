b0e55df6c4c2789b9c6a039a64e3e524f7a91f26
Nicolai Friis <nmfr71398@edu.ucl.dk>
2025-09-20 18:36:19 +0200

    Implementeret følgende: Model af Rent Repository med løs kobling til Rent samt implementeret repository til SQL

---
+++ ReolMarkedWPF/Repositories/IRentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReolMarkedWPF.Models;
+
+namespace ReolMarkedWPF.Repositories
+{
+    // Interface til at implementere SQL database som gemmer "rent". Kan også bruges til at implementere memory versioner til UnitTesting
+    // Løst koblet interface, som henviser til en klasse den bruger, som man definere når interfaced bliver implementeret
+    public interface IRentRepository<T> where T : class
+    {
+        List<T> GetAllRents();
+        void AddRent(T rent);
+        void DeleteRent(T rent);
+        void UpdateRent(T rent);
+    }
+}
