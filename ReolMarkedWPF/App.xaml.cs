6db17a04681c2ff36d3c4b44b16f328cfc0ff468
Thomas Løwe Hansen <tlha71412@edu.ucl.dk>
2025-09-20 14:01:54 +0200

    Missing code added to commit

--- ReolMarkedWPF/App.xaml.cs
+++ ReolMarkedWPF/App.xaml.cs
@@ -1,14 +1,36 @@
-﻿using System.Configuration;
+﻿using ReolMarkedWPF.Repositories;
+using ReolMarkedWPF.ViewModel;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace ReolMarkedWPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            // 1. Definer connection string
+            string connectionString = "Database-Connection-String";
+
+            // 2. Opret instanser af konkrete repositories
+            IShelfVendorRepository shelfVendorRepository = new SqlShelfVendorRepository(connectionString);
+            // Opret andre repositories her...
+
+            // 3. Opret ViewModel og injicer repository-interfacet
+            var shelfVendorViewModel = new ShelfVendorViewModel(shelfVendorRepository);
+
+            // 4. Opret MainWindow
+            var mainWindow = new MainWindow();
+
+            // 5. Sæt ViewModel som DataContext for MainWindow
+            // View og ViewModel forbindes her
+            mainWindow.DataContext = shelfVendorViewModel;
+
+            // 6. Vis vinduet
+            mainWindow.Show();
+        }
+    }
 }
