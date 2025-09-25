014df2caa326606bc02609d658d75690563c171d
Freja <fbfl71372@edu.ucl.dk>
2025-09-25 17:03:07 +0200

    Oprettet fil.

---
+++ ReolMarkedWPF/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarkedWPF.ViewModels
+{
+    internal class ProductViewModel
+    {
+
+    }
+}
