991dbc616465368b924eb8418ffd3c3c0c3b117c
Freja <fbfl71372@edu.ucl.dk>
2025-09-25 22:09:55 +0200

    Interface tilføjet.

--- ReolMarkedWPF/Repositories/IProductRepository.cs
+++ ReolMarkedWPF/Repositories/IProductRepository.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReolMarkedWPF.Models;
 
 namespace ReolMarkedWPF.Repositories
 {
     internal interface IProductRepository
     {
-
+        public List<Product> GetAllProducts();
+        public void AddProduct(Product product);
+        public void DeleteProduct(Product product);
+        public void UpdateProduct(Product product);
     }
 }
