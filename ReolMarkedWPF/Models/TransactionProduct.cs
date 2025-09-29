980c468890b6ebfd7616a2f7f4f2557725d3ef09
Thomas Løwe Hansen <tlha71412@edu.ucl.dk>
2025-09-29 16:05:05 +0200

    Implement OnPropertyChanged for TransactionProduct

--- ReolMarkedWPF/Models/TransactionProduct.cs
+++ ReolMarkedWPF/Models/TransactionProduct.cs
@@ -1,13 +1,25 @@
-﻿namespace ReolMarkedWPF.Models
+﻿using ReolMarkedWPF.Helpers;
+
+namespace ReolMarkedWPF.Models
 {
-
-    //Model til TransactionProduct
-
-    public class TransactionProduct
+    // Klassen arver fra ViewModelBase for at kunne notificere UI'en om ændringer.
+    // Ikke ren "løs kobling", men nødvendig for data binding i WPF.
+    public class TransactionProduct : ViewModelBase
     {
+        private int _amount;
+
         public int TransactionID { get; set; }
         public int ProductID { get; set; }
         public decimal UnitPrice { get; set; }
-        public int Amount { get; set; }
+
+        public int Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged(); // Giver besked til UI, når antallet ændres
+            }
+        }
     }
 }
